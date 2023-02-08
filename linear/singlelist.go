dba3e6093f30972febfc074743b05b3c74b0fccc
cavan.xu <3395058394@qq.com>
2023-02-08 18:35:52 +0800

    feature: singlelist

--- linear/singlelist.go
+++ linear/singlelist.go
@@ -37,3 +37,32 @@ func (lst *SingleList) Add(n *SingleListNode) {
 
 	node.SetNext(n)
 }
+
+func (lst *SingleList) Get(val int) *SingleListNode {
+	lst.RLock()
+	defer lst.RUnlock()
+
+	node := lst.tail
+	for node != nil {
+		if node.Val == val {
+			return node
+		}
+		node = node.Next
+	}
+
+	return nil
+}
+
+func (lst *SingleList) Remove(val int) {
+	lst.Lock()
+	defer lst.Unlock()
+
+	node := lst.tail
+	for node.Next != nil {
+		if node.Next.Val == val {
+			node.Next = node.Next.Next
+			return
+		}
+		node = node.Next
+	}
+}
